f92940d320025f766e82f5985d86809c4e454db8
iTz-Chris <136290447+iTz-Chris@users.noreply.github.com>
2025-02-26 16:10:25 -0300

    olaMundo.c atualizado

--- olaMundo.c
+++ olaMundo.c
@@ -1,8 +1,10 @@
 #include <stdio.h> // Inclui biblioteca para entrada e saída de dados
 
-int main () {
+int main () { // Onde o programa começa a execução
 
+    // Exibe a mensagem na tela. \n adiciona uma nova linha ao final da mensagem
     printf ("Olá Mundo, estou começando a programar em C!\n");
-    return 0;
+
+    return 0; // Indica que o programa terminou com sucesso
 
 }
